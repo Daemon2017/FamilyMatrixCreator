ff9bf41e8f896b50675625f0c92bf2c2142afdd6
Dmitrij Rusakov <daemon2017@gmail.com>
2019-05-19 12:40:08 +0300

    Убраны неправильные автотесты

--- FamilyMatrixCreator/IntegrationsTests.cs
+++ FamilyMatrixCreator/IntegrationsTests.cs
@@ -200,30 +200,6 @@ namespace FamilyMatrixCreator
             {
                 new float[][]
                 {
-                    new float[] {0, 2, 5, 9, 5},
-                    new float[] {0, 0, 0, 5, 0},
-                    null,
-                    null,
-                    null
-                },
-                new int[][]
-                {
-                    new int[] {1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    new int[] {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    null,
-                    null,
-                    null
-                },
-                new List<int> {0, 1, 2, 3, 4},
-                1,
-                new List<int> {2, 3, 4},
-                2,
-                new int[] {0}
-            },
-            new object[]
-            {
-                new float[][]
-                {
                     new float[] {0, 6, 6, 6, 10},
                     new float[] {0, 0, 4, 0, 0},
                     new float[] {0, 0, 0, 0, 0},
@@ -339,31 +315,7 @@ namespace FamilyMatrixCreator
                 new List<int> {2, 3, 4},
                 1,
                 new int[] {7}
-            },
-            new object[]
-            {
-                new float[][]
-                {
-                    new float[] {0, 11, 9, 10, 11},
-                    new float[] {0, 0, 0, 0, 0},
-                    new float[] {0, 0, 0, 0, 0},
-                    null,
-                    null
-                },
-                new int[][]
-                {
-                    new int[] {0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                    null,
-                    null
-                },
-                new List<int> {0, 2, 1, 3, 4},
-                2,
-                new List<int> {2, 3, 4},
-                0,
-                new int[] {0}
-            },
+            },            
             new object[]
             {
                 new float[][]
