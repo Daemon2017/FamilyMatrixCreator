b5a64c43fdb59e30977786e985d9931bb0ecf3aa
Dmitrij Rusakov <daemon2017@gmail.com>
2019-09-18 23:51:27 +0300

    Добавлен класс для хранения свойств родственника

---
+++ FamilyMatrixCreator/Relative.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace FamilyMatrixCreator
+{
+    public class Relative
+    {
+        public int RelativeNumber { get; set; }
+        public RelationshipDegree RelationshipDegree { get; set; }
+        public List<Relative> ParentsList { get; set; }
+        public List<Relative> DescendantsList { get; set; }
+
+        public Relative(int relativeNumber, RelationshipDegree relationshipDegree, 
+            List<Relative> parentsList, List<Relative> descendantsList)
+        {
+            RelativeNumber = relativeNumber;
+            RelationshipDegree = relationshipDegree;
+            ParentsList = parentsList;
+            DescendantsList = descendantsList;
+        }
+    }
+}
